e8389cb02095536ed7058954aa0b8901b89b9990
Gomez0015 <oscargomez.raxo@gmail.com>
2022-05-05 20:53:38 +0200

    Add most of the websites functionality on the discordBot

--- discordBot/events/ready.js
+++ discordBot/events/ready.js
@@ -6,6 +6,6 @@ module.exports = {
         console.log(`${bot.user.username} is online and ready to fly!`);
 
         //Set the Presence of the bot user
-        bot.user.setPresence({ activities: [{ name: 'Hacking into the mainframe!' }] });
+        bot.user.setPresence({ activities: [{ name: 'Hacking into the mainframe!, Prefix: ?' }] });
     }
 }
