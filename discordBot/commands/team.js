f54c418fad912a39c8669021b22b42bb26436352
Gomez0015 <oscargomez.raxo@gmail.com>
2022-05-05 20:38:20 +0200

    Remove useless packages

---
+++ discordBot/commands/team.js
@@ -0,0 +1,44 @@
+const users = require('./models/userModel');
+const teams = require('./models/teamModel');
+const challenges = require('./models/challengeModel');
+const { MessageEmbed } = require('discord.js');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+exports.run = async(bot, message, args) => {
+    if (!(message.guild === null)) return message.reply("This command only works in dms!");
+
+    let checkUser = await users.findOne({ discordId: message.author.id });
+
+    if (checkUser) {
+
+        let userTeam;
+        if (ObjectId.isValid(checkUser.teamId)) {
+            userTeam = await teams.findById(checkUser.teamId);
+        }
+
+        if (userTeam) {
+
+            const teamEmbed = new MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle('EZ CTF | Team')
+                .setTimestamp()
+                .setFooter({ text: 'Raxo#0468' });
+
+            userTeam.users.map((users, index) => {
+                scoreboardEmbed.addField(`${index + 1}. ${user.username}`, user.score.toString(), true);
+            });
+
+            message.reply({ embeds: [teamEmbed] });
+        } else {
+            message.reply("Not in a team!");
+        }
+
+    } else {
+        message.reply("Your discord is not linked to any account!");
+    }
+}
+
+exports.info = {
+    name: "team",
+    description: "See your current team! (Use in DMs)"
+}
