a2533a9e963a27ceb36dd171ba2d07a430f0daff
Gomez0015 <oscargomez.raxo@gmail.com>
2022-05-05 20:06:10 +0200

    Remove useless packages

---
+++ discordBot/commands/score.js
@@ -0,0 +1,22 @@
+const users = require('./models/userModel');
+const teams = require('./models/teamModel');
+const challenges = require('./models/challengeModel');
+const { MessageEmbed } = require('discord.js');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+exports.run = async(bot, message, args) => {
+    if (!(message.guild === null)) return message.reply("This command only works in dms!");
+
+    let checkUser = await users.findOne({ discordId: message.author.id });
+
+    if (checkUser) {
+        message.reply("Your score: " + checkUser.score);
+    } else {
+        message.reply("Your discord is not linked to any account!");
+    }
+}
+
+exports.info = {
+    name: "score",
+    description: "See your current score!"
+}
