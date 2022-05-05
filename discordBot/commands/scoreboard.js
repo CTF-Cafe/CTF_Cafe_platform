7cf45e2d90e70c51448a8e7fd1b625b3a8d5826f
Gomez0015 <oscargomez.raxo@gmail.com>
2022-05-05 20:02:42 +0200

    Remove useless packages

--- discordBot/commands/scoreboard.js
+++ discordBot/commands/scoreboard.js
@@ -2,17 +2,45 @@ const { MessageEmbed } = require('discord.js');
 const users = require('./models/userModel');
 
 exports.run = async(bot, message, args) => {
-    const top = await users.find().sort({ score: -1, _id: 1 }).limit(24);
+    if (!args[0]) return message.reply('Please specify (users/teams) as the first argument.');
 
-    const scoreboardEmbed = new MessageEmbed()
-        .setColor('#ff0000')
-        .setTitle('EZ CTF | Scoreboard')
-        .setTimestamp()
-        .setFooter({ text: 'Raxo#0468' });
+    if (args[0] === 'users') {
+        const top = await users.find().sort({ score: -1, _id: 1 }).limit(24);
 
-    top.map((user, index) => scoreboardEmbed.addField(`${index + 1}. ${user.username}`, user.score.toString(), true))
+        const scoreboardEmbed = new MessageEmbed()
+            .setColor('#ff0000')
+            .setTitle('EZ CTF | Scoreboard')
+            .setTimestamp()
+            .setFooter({ text: 'Raxo#0468' });
 
-    message.reply({ embeds: [scoreboardEmbed] });
+        top.map((user, index) => scoreboardEmbed.addField(`${index + 1}. ${user.username}`, user.score.toString(), true))
+
+        message.reply({ embeds: [scoreboardEmbed] });
+    } else if (args[0] === 'teams') {
+        const top = await teams.find().sort({ 'users.score': -1, _id: 1 }).limit(24);
+
+        const scoreboardEmbed = new MessageEmbed()
+            .setColor('#ff0000')
+            .setTitle('EZ CTF | Scoreboard')
+            .setTimestamp()
+            .setFooter({ text: 'Raxo#0468' });
+
+        top.map((team, index) => {
+            team.users.forEach((user) => {
+                if (team.totalScore) {
+                    team.totalScore += user.score;
+                } else {
+                    team.totalScore = user.score;
+                }
+            });
+
+            scoreboardEmbed.addField(`${index + 1}. ${team.name}`, team.totalScore.toString(), true);
+        });
+
+        message.reply({ embeds: [scoreboardEmbed] });
+    } else {
+        message.reply('Please specify (users/teams) as the first argument.');
+    }
 }
 
 exports.info = {
