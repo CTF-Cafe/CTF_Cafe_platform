7a6518b873f43bb78ba1d3d5e6e99031b9ad80d8
Gomez0015 <oscargomez.raxo@gmail.com>
2022-05-08 14:44:05 +0200

    Adding case insensitive flags

--- backEnd/controllers/adminController.js
+++ backEnd/controllers/adminController.js
@@ -35,6 +35,21 @@ exports.getStats = async function(req, res) {
 
 }
 
+accentsTidy = function(s) {
+    var r = s.toLowerCase();
+    r = r.replace(new RegExp("[àáâãäå]", 'g'), "a");
+    r = r.replace(new RegExp("æ", 'g'), "ae");
+    r = r.replace(new RegExp("ç", 'g'), "c");
+    r = r.replace(new RegExp("[èéêë]", 'g'), "e");
+    r = r.replace(new RegExp("[ìíîï]", 'g'), "i");
+    r = r.replace(new RegExp("ñ", 'g'), "n");
+    r = r.replace(new RegExp("[òóôõö]", 'g'), "o");
+    r = r.replace(new RegExp("œ", 'g'), "oe");
+    r = r.replace(new RegExp("[ùúûü]", 'g'), "u");
+    r = r.replace(new RegExp("[ýÿ]", 'g'), "y");
+    return r;
+};
+
 exports.saveChallenge = async function(req, res) {
     const challengeExists = await challenges.findById((req.body.id));
 
