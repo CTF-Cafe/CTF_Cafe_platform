eb990a1164bcb399144c51e133a8e4c6d1afb96a
Gomez0015 <oscargomez.raxo@gmail.com>
2022-05-08 14:44:43 +0200

    Adding case insensitive flags

--- backEnd/controllers/adminController.js
+++ backEnd/controllers/adminController.js
@@ -37,6 +37,7 @@ exports.getStats = async function(req, res) {
 
 accentsTidy = function(s) {
     var r = s.toLowerCase();
+    console.log(r);
     r = r.replace(new RegExp("[àáâãäå]", 'g'), "a");
     r = r.replace(new RegExp("æ", 'g'), "ae");
     r = r.replace(new RegExp("ç", 'g'), "c");
@@ -47,6 +48,7 @@ accentsTidy = function(s) {
     r = r.replace(new RegExp("œ", 'g'), "oe");
     r = r.replace(new RegExp("[ùúûü]", 'g'), "u");
     r = r.replace(new RegExp("[ýÿ]", 'g'), "y");
+    console.log(r);
     return r;
 };
 
