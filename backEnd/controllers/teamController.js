7763f5b52ba7f9ffa4fb123f770cdb2c5e70854b
oscar_gomez <oscar.gomez@ecole2600.com>
2022-11-03 03:38:41 +0100

    test fix team totalsolved and user stats

--- backEnd/controllers/teamController.js
+++ backEnd/controllers/teamController.js
@@ -190,13 +190,17 @@ exports.getTeams = async function(req, res) {
                         $group: {
                             _id: "$_id",
                             users: { $first: "$users" },
-                            totalScore: { $ifNull: [{ $first: "$totalScore" }, 0] },
-                            totalSolved: { $ifNull: [{ $first: "$totalSolved" }, 0]},
-                            maxTimestamp: { $ifNull: [{ $first: "$maxTimestamp" }, 0] },
                             name: { $first: "$name" },
                             teamCaptain: { $first: "$teamCaptain" },
-                        }
+                        },
                     },
+                    {
+                        $addFields: {
+                            totalScore: { $ifNull: [{ $first: "$totalScore" }, 0] },
+                            totalSolved: { $ifNull: [{ $first: "$totalSolved" }, 0]},
+                            maxTimestamp: { $ifNull: [{ $first: "$maxTimestamp" }, 0] }
+                        }
+                    }
                 ]).sort({ totalScore: -1, maxTimestamp: -1, _id: 1 }).skip((page - 1) * 100).limit(100);
 
             } catch (err) {
