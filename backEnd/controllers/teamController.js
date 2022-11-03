68f974b85ce0b15a1cf1776fcf2e0cf5b78bbac5
oscar_gomez <oscar.gomez@ecole2600.com>
2022-11-03 03:26:23 +0100

    test fix team totalsolved and user stats

--- backEnd/controllers/teamController.js
+++ backEnd/controllers/teamController.js
@@ -180,7 +180,7 @@ exports.getTeams = async function(req, res) {
                             _id: "$_id",
                             users: { $first: "$users" },
                             totalScore: { $sum: "$newSolved.points" },
-                            totalSolved: { $sum: 1 },
+                            totalSolved: { $count: "$newSolved" },
                             maxTimestamp: { $max: "$newSolved.timestamp" },
                             name: { $first: "$name" },
                             teamCaptain: { $first: "$teamCaptain" },
