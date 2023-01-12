9ed9f4849e3123b1f4a692d943cc06fd252ed5b6
oscar_gomez <you@example.com>
2023-01-12 17:19:43 +0100

    added email verification

--- backEnd/controllers/challengesController.js
+++ backEnd/controllers/challengesController.js
@@ -328,7 +328,7 @@ exports.submitFlag = async function (req, res) {
         const dynamicScoring = await ctfConfig.findOne({ name: 'dynamicScoring' });
 
         if(dynamicScoring.value.toString() == "true") {
-            const decay = (await users.countDocuments()) * 0.18;
+            const decay = (await teams.countDocuments()) * 0.18;
             let dynamicPoints = Math.ceil((((challenge.minimumPoints - challenge.initialPoints)/((decay**2)+1)) * ((challenge.solveCount+1)**2)) + challenge.initialPoints)
             if(dynamicPoints < challenge.minimumPoints) { dynamicPoints = challenge.minimumPoints }
 
