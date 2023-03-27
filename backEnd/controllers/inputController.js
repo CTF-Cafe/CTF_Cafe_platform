79388bfa88450fc616b9d508963393926b6841db
Gomez0015 <oscargomez.raxo@gmai.com>
2023-03-28 00:26:10 +0200

    Creating Validation Function

---
+++ backEnd/controllers/inputController.js
@@ -0,0 +1,34 @@
+exports.validateRequestBody = function(body, validationObject) {
+    for (const [key, validation] of Object.entries(validationObject)) {
+      const value = body[key];
+      if (validation.required && (value === undefined || value === '')) {
+        throw new Error(`${key} is required`);
+      }
+      if (validation.type === 'number' && isNaN(value)) {
+        throw new Error(`${key} must be a number`);
+      }
+      if (validation.type === 'positiveNumber' && (isNaN(value) || parseInt(value) < 0)) {
+        throw new Error(`${key} must be a positive number`);
+      }
+      if (validation.type === 'objectId' && (value !== '' && !ObjectId.isValid(value))) {
+        throw new Error(`${key} must be a valid ObjectId`);
+      }
+      if (validation.type === 'array') {
+        const parsedArray = JSON.parse(value);
+        parsedArray.forEach((item, index) => {
+          for (const [itemKey, itemValidation] of Object.entries(validation.itemValidation)) {
+            const itemValue = item[itemKey];
+            if (itemValidation.required && (itemValue === undefined || itemValue === '')) {
+              throw new Error(`Item ${index} of ${key} must have ${itemKey} defined`);
+            }
+            if (itemValidation.type === 'number' && isNaN(itemValue)) {
+              throw new Error(`Item ${index} of ${key} ${itemKey} must be a number`);
+            }
+            if (itemValidation.type === 'positiveNumber' && (isNaN(itemValue) || parseInt(itemValue) < 0)) {
+              throw new Error(`Item ${index} of ${key} ${itemKey} must be a positive number`);
+            }
+          }
+        });
+      }
+    }
+  }
