a3df3a80ff2da80917f50b9cb28b342cc209fe50
oscar_gomez <you@example.com>
2023-05-12 17:14:53 +0200

    remove release

--- backEnd/controllers/encryptionController.js
+++ backEnd/controllers/encryptionController.js
@@ -1,23 +1,11 @@
 // VERY IMPORTANT!! READ: https://www.troyhunt.com/we-didnt-encrypt-your-password-we-hashed-it-heres-what-that-means/
 
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
-// exports.encrypt = async function(pass) {
-//     return await bcrypt.hash(pass, 10);
-// }
-
-// exports.compare = async function(pass, hash) {
-//     return await bcrypt.compare(pass, hash);
-// }
-
-function hashPassword(password) {
-  return require("crypto").createHash("sha256").update(password, "utf8").digest("hex");
+exports.encrypt = async function(pass) {
+    return await bcrypt.hash(pass, 10);
 }
 
-exports.encrypt = async function (pass) {
-  return hashPassword(pass);
-};
-
-exports.compare = async function (pass, hash) {
-  return hashPassword(pass) === hash;
-};
+exports.compare = async function(pass, hash) {
+    return await bcrypt.compare(pass, hash);
+}
